495b2dfc2ca9a03ab411b438643ab11fc2fdc27d
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-17 00:54:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ end
   end 
   
   get '/say/:number/:phrase' do 
-    @numberphrase = params[:number].to_i
+    @numberphrase = params[:number].to_i.times do 
+    return params[:phrase]
     
 end
