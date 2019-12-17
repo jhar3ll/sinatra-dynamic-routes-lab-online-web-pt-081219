44890b253e935db5cee0d9a86d69272eddd64e36
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-17 01:16:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,13 +37,13 @@ end
 
     case params[:operation]
     when 'add'
-      answer = (number1 + number2).to_s
+      answer = (number1 + number2).to_s + "."
     when 'subtract'
-      answer = (number1 - number2).to_s
+      answer = (number1 - number2).to_s + "."
     when 'multiply'
-      answer = (number1 * number2).to_s
+      answer = (number1 * number2).to_s + "."
     when 'divide'
-      answer = (number1 / number2).to_s
+      answer = (number1 / number2).to_s + "."
     end
   end
 end
