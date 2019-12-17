3d9250f9b1281db355f8e377281d2e69f7a42f6f
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-17 01:13:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ end
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
 
-    answer = 'Unable to perform this operation'
+    answer = "error"
 
     case params[:operation]
     when 'add'
