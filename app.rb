701c702df32836d147b3cd56e95ef99414aad77e
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-17 00:43:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name'
+  @reversename = params[:name].reverse
+  "@#{reversename}"
+end
 
 end
